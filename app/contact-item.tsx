4fdc9b87666e67f2e2e2a2bb8070a1e1da677922
Philemon Heng <philemonheng@Philemons-MacBook-Pro.local>
2024-05-09 01:11:39 +0800

    disable delete when others editing

--- app/contact-item.tsx
+++ app/contact-item.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useContext } from 'react'
 
-import { ContactContext } from './contacts'
+import { EditContext, ContactContext } from './contacts'
 
 import { useContactFormField } from './new-contact-form'
 
@@ -24,24 +24,43 @@ export default function ContactItem(props: ContactProps) {
     const phoneNumberProps = useContactFormField(contact.phoneNumber, null)
     const addressProps = useContactFormField(contact.address, null)
 
-    const contactFormFields = [nameProps, phoneNumberProps, addressProps]
+    const contactFormFieldProps = [nameProps, phoneNumberProps, addressProps]
 
     const [editMode, setEditMode] = useState(false)
 
     const { contacts, setContacts } = useContext(ContactContext)
+    const { currEditingContacts, setCurrEditingContacts } = useContext(EditContext)
+
+    const handleEnterEditingMode = () => {
+        setEditMode(true)
+        setCurrEditingContacts([
+            ...currEditingContacts,
+            contact.id,
+        ])
+    }
 
     const handleSave = () => {
         setEditMode(false)
 
-        const indexOfCurrEdit = contacts.findIndex(currContact => currContact.id === contact.id)
+        const indexOfCurrContact = contacts.findIndex(currContact => currContact.id === contact.id)
 
-        const updatedContacts = contacts.toSpliced(indexOfCurrEdit, 1, {
+        const updatedContacts = contacts.toSpliced(indexOfCurrContact, 1, {
             id: contact.id,
             name: nameProps.value,
             phoneNumber: phoneNumberProps.value,
             address: addressProps.value,
         })
 
+        setContacts(updatedContacts)
+
+        const editingContactsIndexOfCurr = currEditingContacts.findIndex(currEditContId => currEditContId === contact.id)
+        const updatedCurrEditingContacts = currEditingContacts.toSpliced(editingContactsIndexOfCurr, 1)
+        setCurrEditingContacts(updatedCurrEditingContacts)
+    }
+
+    const handleDeleteContact = () => {
+        const indexOfCurrContact = contacts.findIndex(currContact => currContact.id === contact.id)
+        const updatedContacts = contacts.toSpliced(indexOfCurrContact, 1)
         setContacts(updatedContacts)
     }
 
@@ -52,15 +71,20 @@ export default function ContactItem(props: ContactProps) {
                     <div>{contact.name}</div>
                     <div>{contact.phoneNumber}</div>
                     <div>{contact.address}</div>
-                    <button type="button" onClick={() => setEditMode(true)}>Edit</button>
+                    <button type="button" onClick={() => handleEnterEditingMode()}>Edit</button>
+                    <button type="button" disabled={currEditingContacts.length > 0} onClick={() => handleDeleteContact()}>Delete</button>
                 </>
                 : <>
-                    {contactFormFields.map((formField, i) => (
-                        <input
-                            key={i}
-                            {...formField}
-                        />
-                    ))}
+                    {contactFormFieldProps.map((formFieldProps, i) => {
+                        const { reset, ...rest } = formFieldProps
+
+                        return (
+                            <input
+                                key={i}
+                                {...rest}
+                            />
+                        )
+                    })}
                     <button type="button" onClick={handleSave}>Save</button>
                 </>
             }
--- app/contacts.tsx
+++ app/contacts.tsx
@@ -5,7 +5,13 @@ import { useState, createContext, SetStateAction, Dispatch } from 'react';
 import ContactItem, { Contact } from './contact-item'
 
 const defaultContacts: Contact[] = [
-
+    { id: 1, name: 'Bob', phoneNumber: '123', address: 'address1' },
+    { id: 2, name: 'Bob1', phoneNumber: '123', address: 'address1' },
+    { id: 3, name: 'Bob2', phoneNumber: '123', address: 'address1' },
+    { id: 4, name: 'Bob3', phoneNumber: '123', address: 'address1' },
+    { id: 5, name: 'Bob4', phoneNumber: '123', address: 'address1' },
+    { id: 6, name: 'Bob5', phoneNumber: '123', address: 'address1' },
+    { id: 7, name: 'Bob6', phoneNumber: '123', address: 'address1' },
 ]
 
 interface ContactContextType {
@@ -13,29 +19,38 @@ interface ContactContextType {
     setContacts: Dispatch<SetStateAction<Contact[]>>
 }
 
+interface EditContextType {
+    currEditingContacts: number[]
+    setCurrEditingContacts: Dispatch<SetStateAction<number[]>>
+}
+
 export const ContactContext = createContext<ContactContextType>({ contacts: defaultContacts, setContacts: () => { } });
+export const EditContext = createContext<EditContextType>({ currEditingContacts: [], setCurrEditingContacts: () => { } });
 
 import NewContactForm from './new-contact-form'
 export default function Contacts() {
     const [searchVal, setSearchVal] = useState("")
     const [newContactForm, setNewContactForm] = useState(false)
     const [contacts, setContacts] = useState(defaultContacts)
+    const [currEditingContacts, setCurrEditingContacts] = useState<number[]>([])
 
     return (
         <ContactContext.Provider value={{ contacts: contacts, setContacts: setContacts }}>
-            <div className="flex flex-col">
-                <button type="button" onClick={() => setNewContactForm(true)}>Add new contact</button>
-                <input type="text" placeholder="Search for contact..." value={searchVal} onChange={(event) => setSearchVal(event.target.value)}></input>
-                {contacts
-                    .filter((contact: Contact) => (
-                        contact.name.toUpperCase().includes(searchVal.toUpperCase())
-                        || searchVal.toUpperCase().includes(contact.name.toUpperCase()
-                        )))
-                    .map((contact: Contact, i) => (
-                        <ContactItem key={contact.id + contact.name + i} contact={contact} />
-                    ))}
-                {newContactForm && <NewContactForm />}
-            </div>
+            <EditContext.Provider value={{ currEditingContacts, setCurrEditingContacts }}>
+                <div className="flex flex-col">
+                    <button type="button" onClick={() => setNewContactForm(true)}>Add new contact</button>
+                    <input type="text" placeholder="Search for contact..." value={searchVal} onChange={(event) => setSearchVal(event.target.value)}></input>
+                    {contacts
+                        .filter((contact: Contact) => (
+                            contact.name.toUpperCase().includes(searchVal.toUpperCase())
+                            || searchVal.toUpperCase().includes(contact.name.toUpperCase()
+                            )))
+                        .map((contact: Contact, i) => (
+                            <ContactItem key={contact.id + contact.name + i} contact={contact} />
+                        ))}
+                    {newContactForm && <NewContactForm />}
+                </div>
+            </EditContext.Provider>
         </ContactContext.Provider>
     )
 }
